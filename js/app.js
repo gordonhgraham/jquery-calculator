6b9b8d3fe9d3a5f2aea6e89c5b5218ed9026b0ec
Gordon H Graham <theboss@gordonhgraham.com>
2016-08-21 15:52:54 -0600

    update app.js--calculator now works with longer operands

--- js/app.js
+++ js/app.js
@@ -1,58 +1,67 @@
 var a = null;
 var b = null;
-var operand = "";
+var symbol = "";
 
+//event listener for buttons
 $('.buttons').on('click', function() {
     if ($(event.target).text() === "C") {
         clearScreen();
     } else if ($('#screen').text() === "Error") {
         return;
     } else if ($(event.target).text() === "=") {
-        evaluate(a, b, operand);
+        b = $("#screen").text();
+        evaluate(a, b, symbol);
         displayResult();
     } else {
         appendScreen();
     }
 });
 
+//append screen and save inputs to variables
 function appendScreen() {
     var input = $(event.target).text();
-    $('#screen').append(input);
     if ($(event.target).hasClass('operator')) {
-        operand = $(event.target).text();
+        a = $('#screen').text();
+        $('#screen').text("");
+        $('#screen').append(input);
+        symbol = $(event.target).text();
     } else {
         if (a === null) {
-            a = $(event.target).text();
+            $('#screen').append(input);
         } else {
+            $('#screen').append(input);
             b = $(event.target).text();
         }
     }
     return input;
 }
 
+//clears user screen and sets variables to zero
 function clearScreen() {
     //set variables to null
     a = null;
     b = null;
-    operand = null;
+    symbol = null;
     //clear screen
     $('#screen').text('');
     return;
 }
 
+
+//displays result and resets variables
 function displayResult() {
     //display result
     $('#screen').text(result);
     //set variables to null
     a = null;
     b = null;
-    operand = null;
+    symbol = null;
 }
 
-function evaluate(a, b, operand) {
+function evaluate(a, b, symbol) {
     var aInt = parseFloat(a);
-    var bInt = parseFloat(b);
-    switch (operand) {
+    var bInt = parseFloat(b.substring(1, b.length));
+    switch (symbol) {
         case "+":
             result = aInt + bInt;
             break;
@@ -64,9 +73,11 @@ function evaluate(a, b, operand) {
             break;
         case "÷":
             result = aInt / bInt;
+            if (bInt === 0) {
+                result = "Error";
+            }
             break;
         default:
-
     }
     return result;
 }
